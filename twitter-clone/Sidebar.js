0573156255a4b49db5a0340bc627e4dc4ab00900
atere21 <tosinateret@gmail.com>
2022-10-07 05:32:51 -0500

    twitter-clone-build

---
+++ twitter-clone/Sidebar.js
@@ -0,0 +1,21 @@
+import React from 'react'
+
+function Sidebar() {
+  return (
+
+
+    <div className='sidebar'>
+
+        {/*SidebarOptions*/}
+        {/*SidebarOptions*/}
+        {/*SidebarOptions*/}
+        {/*SidebarOptions*/}
+        {/*SidebarOptions*/}
+        {/*Button*/}
+
+
+    </div>
+  )
+}
+
+export default Sidebar
