3e25d831a91d5cc01de8a4593b8afed3cdf8bb4f
atere21 <tosinateret@gmail.com>
2022-10-10 09:45:59 -0500

    twitter-clone

--- twitter-clone/src/Widgets.js
+++ twitter-clone/src/Widgets.js
@@ -1,10 +1,26 @@
 import React from 'react'
 import './Widgets.css';
+import {
+  TwitterTimelineEmbed,
+  TwitterShareButton,
+  TwitterTweetEmbed,
+} from 'react-twitter-embed';
+import SearchIcon from '@mui/icons-material/Search';
 
 function Widgets() {
   return (
     <div  className='Widgets'>
-        <h2>Widgets</h2>
+        
+        <div className='widgets__inputs'>
+          <SearchIcon className='widgets__searchIcon' />
+          <input placeholder='search Twitter' type='text'/>
+        </div>
+
+        <div className='widgets__widgetcontainer'>
+          <h2>What's happening</h2>
+          <TwitterTweetEmbed tweetId={"1564307731875520512"}></TwitterTweetEmbed>
+
+        </div>
     </div>
   )
 }
