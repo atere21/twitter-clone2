fe4cf03064bbd7022f968417361969f9a8652f63
atere21 <tosinateret@gmail.com>
2022-10-11 07:44:02 -0500

    twitter-clone

--- twitter-clone/src/firebase.js
+++ twitter-clone/src/firebase.js
@@ -9,7 +9,4 @@ const firebaseConfig = {
     appId: "1:393178677642:web:9299509e08750cf62c0873",
     measurementId: "G-FDFJD5J22F"
   };
-  /*const firebaseApp = firebase.initializeApp(firebaseConfig)*/
-  
-  
-  
+  /*const firebaseApp = firebase.initializeApp(firebaseConfig
